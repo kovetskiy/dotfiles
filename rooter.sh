9003767101d5fd2150294f0bc8ef4a43adb37326
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2015-08-06 12:52:25 +0600

    roooter: add zsnap and remove .git/readme.md trash

--- rooter.sh
+++ rooter.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
+set -e -u
+zsnap
+
 src_dir=$(readlink -f root)
 dest_dir="/"
 
-for src_file in `find $src_dir`; do
+for src_file in `find $src_dir | tail -n +2`; do
     dest_file=$dest_dir$(sed "s@$src_dir/@@g" <<< "$src_file")
-    if test -f $src_file; then
-        if test -f $dest_file; then
-            diff -u $src_file $dest_file
+
+    if [[ $dest_file == *.git* ]]; then
+        continue
+    fi
+
+    if [[ $dest_file == *README.* ]]; then
+        continue
+    fi
+
+    mkdir -p "$(dirname "$dest_file")"
+    if test -f "$src_file"; then
+        if test -f "$dest_file"; then
+            diff -u "$src_file" "$dest_file"
             if [ $? -ne 0 ]; then
                 exit 1
             fi
         fi
-    fi
 
-    echo cp $src_file $dest_file
-    #cp -ruT $src_file $dest_file
+        echo cp "$src_file" "$dest_file"
+        sudo cp -ruT $src_file $dest_file
+    fi
 done
