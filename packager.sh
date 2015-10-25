02681f8be3c921710aa5b9f0356b15e9b27f50ed
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2015-10-25 17:07:38 +0600

    packager: improve speed

--- packager.sh
+++ packager.sh
@@ -10,7 +10,7 @@ yaourt -Sy
 
 for package in `cat packages`; do
     echo checking package $package...
-    installed=$(pacman -Qi $package)
+    installed=$(pacman -Q $package)
     if [ $? -ne 0 ]; then
         if [[ "$1" -ne "-f" ]];then
             echo installing package $package...
