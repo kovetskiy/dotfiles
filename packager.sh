560e1592f7bd8a1f55709ed97cd28e41e55d893b
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2016-04-15 22:09:48 +0600

    packages updated

--- packager.sh
+++ packager.sh
@@ -10,9 +10,9 @@ yaourt -Sy
 
 for package in `cat packages`; do
     echo checking package $package...
-    installed=$(pacman -Q $package)
+    installed=$(pacman -Q $package 2>&1)
     if [ $? -ne 0 ]; then
-        if [[ "$1" -ne "-f" ]];then
+        if [ "$1" != "-s" ];then
             echo installing package $package...
             install $package
         fi
