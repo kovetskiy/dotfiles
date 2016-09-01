272b0c7e96fb091d6061d510fd072e128afbed2d
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2016-09-01 13:11:49 +0700

    vimperator: come back

---
+++ .vimperator/mappings.js
@@ -0,0 +1,36 @@
+qmark h http://news.ycombinator.com/
+qmark g http://github.com/
+qmark u http://mail.google.com/
+qmark s https://ngs-team.slack.com/messages/devops
+qmark p https://postdevops.slack.com/messages/general
+qmark d https://postdevops.slack.com/messages/dead
+
+map U gT
+map I gt
+
+nmap g     :tabopen http://google.ru/search?q=
+nmap x     :tabopen http://slovari.yandex.ru/
+nmap q     <ESC>
+nmap s     :tabopen<space>
+nmap b     :buffer<space>
+nmap <C-P> :set apptab=true<CR>:echomsg "Pinned"<CR>
+nmap <C-N> :set apptab=false<CR>:echomsg "Unpinned"<CR>
+nmap v     cv
+nmap w     gT
+nmap e     gt
+nmap .     /
+
+imap <C-}> <C-I>
+imap jk    <ESC>
+
+vmap c     <ESC><ESC>c
+
+nnoremap j   5j
+nnoremap k   5k
+nnoremap ee  :buffer<space>
+nnoremap %   :source ~/.vimperatorrc<CR>
+
+inoremap <C-]> <Ins><C-a><Ins><C-I>
+inoremap <C-a> <Ins><C-a><Ins>
+
+" vim:ft=vim
---
+++ .vimperator/settings.js
@@ -0,0 +1,32 @@
+set! hintchars=qweasdiopjkl
+set! gui=none
+
+set! editor="vimperator-editor $0"
+set! cpt=l
+set! showstatuslinks=2
+set! hto=170
+set! focuscontent
+
+set! breakpad.reportURL=""
+set! browser.safebrowsing.appRepURL=""
+set! browser.safebrowsing.debug=false
+set! browser.safebrowsing.downloads.enabled=false
+set! browser.safebrowsing.downloads.remote.enabled=false
+set! browser.safebrowsing.enabled=false
+set! browser.safebrowsing.gethashURL=""
+set! browser.safebrowsing.malware.enabled=false
+set! browser.safebrowsing.malware.reportURL=""
+set! browser.safebrowsing.reportErrorURL=""
+set! browser.safebrowsing.reportGenericURL=""
+set! browser.safebrowsing.reportMalwareErrorURL=""
+set! browser.safebrowsing.reportMalwareURL=""
+set! browser.safebrowsing.reportPhishURL=""
+set! browser.safebrowsing.reportURL=""
+set! browser.safebrowsing.updateURL=""
+set! browser.send_pings.require_same_host=true
+set! browser.send_pings=false
+set! javascript.options.jit.chrome=true
+set! signon.rememberSignons=false
+set! browser.tabs.closeWindowWithLastTab=false
+
+" vim:ft=vim
